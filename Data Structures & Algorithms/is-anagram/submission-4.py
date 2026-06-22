c1f76860271d0a5be41091b607523c7b2dbf5c20
RakeshM7 <42072043+RakeshM7@users.noreply.github.com>
2026-06-22 21:44:28 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,19 @@
+class Solution:
+
+    # 1 testcase failing for long inputs
+    def sortStrings(self, input: str) -> str:
+        input_arr = list(input)
+        for i in range(1, len(input_arr)):
+            curr_element = input_arr[i]
+            j = i-1
+            while(j>=0 and input_arr[j] > curr_element):
+                input_arr[j+1] = input_arr[j]
+                j = j - 1
+            input_arr[j+1] = curr_element
+        return ''.join(input_arr)
+
+    # by sorting
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+        
